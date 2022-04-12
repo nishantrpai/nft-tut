2ecf759c776dd502df848fa8a9d136394230dc17
botvader <eiglesias914@gmail.com>
2022-04-12 17:27:44 +0530

    change contract address

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -7,7 +7,7 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
 
-const contractAddress = "0x0d8894eDaE976Fe87f6b30b172524c34390341Af"
+const contractAddress = "0xC51ccf34AAa82804b02A238260891b7260F01642"
 
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
