23dcbbc846db9417be5beb39b7e5079618c8e5dd
nishantrpai <nishantrpai@gmail.com>
2022-05-09 13:13:24 +0530

    change gold to keys

--- contracts/NFTVault.sol
+++ contracts/NFTVault.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
-import "./GLDToken.sol";
+import "./KEYToken.sol";
 
 contract NFTVault is IERC721Receiver {
     struct NFT {
         address contractAddress;
         uint256 tokenId;
     }
-    // gold for erc20
-    GLDToken gold;
+    // keys for erc20
+    KEYToken keys;
     address payable public owner;
     uint256 public TOKEN_AMOUNT = 100;
     //
@@ -23,29 +23,20 @@ contract NFTVault is IERC721Receiver {
     // contract array
 
     constructor() public {
-        gold = new GLDToken("Gold", "GLD", 1000000);
+        keys = new KEYToken("KEYS", "KEY", 1000000);
         owner = payable(msg.sender);
     }
 
-    function transferGold(
-        address _from,
-        address _to,
-        uint256 amount
-    ) public returns (bool success) {
-        gold.approve(address(this), amount);
-        return gold.transferFrom(_from, _to, amount);
-    }
-
     function getVaultBalance() public view returns (uint256 balance) {
-        return gold.balanceOf(address(this));
+        return keys.balanceOf(address(this));
     }
 
     function getCurrentBalance() public view returns (uint256 balance) {
-        return gold.balanceOf(msg.sender);
+        return keys.balanceOf(msg.sender);
     }
 
     function getTokenAddr() public view returns (address token) {
-        return address(gold);
+        return address(keys);
     }
 
     function setWhiteList(address _addr, bool _whiteList) public {
@@ -57,11 +48,11 @@ contract NFTVault is IERC721Receiver {
         public
         returns (bool success)
     {
-        // console.log("balance %s", gold.balanceOf(msg.sender));
-        require(gold.balanceOf(msg.sender) != 0, "sender cannot have 0 gold");
+        // console.log("balance %s", keys.balanceOf(msg.sender));
+        require(keys.balanceOf(msg.sender) != 0, "sender cannot have 0 keys");
         require(nfts.length > 0, "no more nfts");
         require(amount == TOKEN_AMOUNT, "amount must be 100");
-        if (gold.transferFrom(msg.sender, address(this), amount)) {
+        if (keys.transferFrom(msg.sender, address(this), amount)) {
             // get random nft
             uint256 randomIndex = uint256(
                 keccak256(abi.encodePacked(block.difficulty, msg.sender))
@@ -99,7 +90,7 @@ contract NFTVault is IERC721Receiver {
         uint256 _tokenId,
         bytes memory
     ) public virtual override returns (bytes4) {
-        if (gold.transfer(_from, TOKEN_AMOUNT) && whiteList[msg.sender]) {
+        if (keys.transfer(_from, TOKEN_AMOUNT) && whiteList[msg.sender]) {
             //first time new contract is showing up
             nfts.push(NFT(msg.sender, _tokenId));
             return this.onERC721Received.selector;
